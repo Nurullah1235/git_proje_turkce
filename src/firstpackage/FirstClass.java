6637f9b35fec07db67cc0fc01bc9f7d7e75096a1
nurullah <berat1000gol012@gmail.com>
2020-06-20 22:28:10 +0300

    comflict olusturma

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -3,11 +3,11 @@ package firstpackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
-		System.out.println("third commit from local repository");
+		System.out.println("ucuncu commitimiz local repositoryden");
 		System.out.println("first commit from remote repository");
 		
 	
