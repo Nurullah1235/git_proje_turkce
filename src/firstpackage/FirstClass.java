0e54a44ea131b3516b2202c23f2a578cb0312a15
nurullah <berat1000gol012@gmail.com>
2020-06-20 21:14:04 +0300

    first commit from local repo

---
+++ src/firstpackage/FirstClass.java
@@ -0,0 +1,17 @@
+package firstpackage;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("first commit from local repository");
+	
+	
+	
+	
+	
+	
+	}
+
+}
